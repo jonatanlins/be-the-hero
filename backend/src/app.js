f42c0e1caad4b6930205c5e660a9b534360c39c4
Jonatan Lins <jonatanpinheiro99@gmail.com>
2020-03-31 23:09:49 -0300

    teste iniciados

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,10 @@ const routes = require("./routes");
 const { errors } = require("celebrate");
 
 const app = express();
-const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
--- backend/src/controllers/OngProfileController.js
+++ backend/src/controllers/OngProfileController.js
@@ -1,5 +1,4 @@
 const db = require("../database/connection");
-const crypto = require("crypto");
 
 module.exports = {
   async index(request, response) {
